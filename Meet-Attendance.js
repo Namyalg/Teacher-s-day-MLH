d7e41d882bc595d40959dfb7eb48c4492c338d4d
Namyalg <namyalg@gmail.com>
2021-09-04 21:06:03 +0530

    Retrieved the names and times from the chat

--- Meet-Attendance.js
+++ Meet-Attendance.js
@@ -33,34 +33,47 @@ function startIt(message, sender, sendResponse){
         var hours = today.getHours();
         var file_name = "Attendance:" + hours + "hrs-" + day +"-" + month + ".txt"  ;
         alert("in take attendence");
+
+
 		var names = document.querySelectorAll('div[data-self-name="You"]');
 
         //#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6
         let elements = document.getElementsByClassName('z38b6');
+        
+        //let elements = document.getElementsByClassName('YTbUZc');
+        
+
+
+        console.log(elements);
+
+        console.log(document.querySelectorAll('.YTbUzc'));
+
+        var names = document.querySelectorAll('.YTbUzc');
+        var times = document.querySelectorAll('.MuzmKe');
+
+        for(var i = 0; i < names.length; i++){
+            console.log(names[i].innerHTML);
+            console.log(times[i].innerHTML);
+        }
+
+
+
+    
+
+
         //var senderElement = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6")
         var senderElement = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6")
         
-        //console.log(senderElement.querySelector('div.YTbUZc'));
-        //var descendents = elements.getElementsByTagName('*');
-        console.log(senderElement);
-        
-        // for( el in senderElement)
-        // {
-        //     console.log(el);
-           
-        // }
-
-        //var senderElement = document.querySelectorAll('div:nth-child(2)')[1]
-       
+      
         
         
-        var messageElement = document.querySelector('div:nth-child(3)')
+        // var messageElement = document.querySelector('div:nth-child(3)')
 
-        const commentList = document.querySelector('[jscontroller=ENYfP]')
-        console.log("sender is ");
-        console.log(senderElement);
+        // const commentList = document.querySelector('[jscontroller=ENYfP]')
+        // console.log("sender is ");
+        // console.log(senderElement);
 
-        console.log(commentList);
+        // console.log(commentList);
 
         ////*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[4]/div[2]/div[2]/div/div[2]
         //<div jsname="xySENc" class="z38b6" aria-live="polite"><div class="v8W0vf">Messages can only be seen by people in the call and are deleted when the call ends.</div><div class="GDhqjd" style="order: 1;" jsname="Ypafjf" tabindex="-1" jscontroller="LQRnv" jsaction="JIbuQc:cut0F(aUCive),dcNHe(g21v4c);" data-sender-id="_self_" data-sender-name="You" data-timestamp="1630689276999" data-formatted-timestamp="10:44 PM"><div><div class="YTbUzc">You</div><div class="MuzmKe">10:44 PM</div></div><div class="Zmm6We" jsname="h83qlf"><div class="oIy2qc" jsname="dTKtvb" data-message-text="hello">hello</div><div class="oIy2qc" jsname="dTKtvb" data-message-text="hello">hello</div><div class="oIy2qc" jsname="dTKtvb" data-message-text="hello">hello</div><div class="oIy2qc" jsname="dTKtvb" data-message-text="hello">hello</div></div></div></div>
